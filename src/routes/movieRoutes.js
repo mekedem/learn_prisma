04faeec11f212282d95a605d937e209102556023
Mekedem Getaneh <mekedemgetaneh@gmail.com>
2026-05-15 21:10:05 +0300

    removed commented thing

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -6,7 +6,4 @@ router.get('/hello', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
-export default router;  
-
-// postgresql://neondb_owner:npg_zhAOJ3j8ryQN@ep-noisy-cell-aquehy2e.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require
-// postgresql://neondb_owner:npg_zhAOJ3j8ryQN@ep-noisy-cell-aquehy2e.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require
+export default router;
